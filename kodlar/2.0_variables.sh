f12a728a7d2d2fae769155d426cc94973f04cb2e
pleycpl <h4rvey@protonmail.com>
2017-10-09 20:58:28 +0000

    ek satirlar eklendi.

--- kodlar/2.0_variables.sh
+++ kodlar/2.0_variables.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
-integer_degisken=10
-string_degisken="Naber"
+ #!/usr/bin/env bash
+                    # 'integer' ve 'string' degisken isimlerimizdir.
+                    # Bash'te degiskene deger verirlirken '=' arasinda bosluk olmamali.
+integer=10
+string="Naber, Nasilsin?"
 
-echo $integer_degisken    # "integer_degisken" degiskenimizin icindeki degeri ekrana basar.
-echo $string_degisken     # "string_degisken" degiskenimizin icindeki degeri ekrana basar.
-echo ${#string_degisken}  # "integer_degisken" degiskenimizin icindeki degerin uzunlugunu ekrana basar.
-                            # Klasik len() fonksiyonu gibi
+echo $integer       # "integer" degiskenimizin icindeki degeri ekrana basar.
+echo $string        # "string" degiskenimizin icindeki degeri ekrana basar.
+echo ${string}      # Bir usteki satirla ayni isi yapar.
+                      # '{}' kullanmamizin sebebi degisken isimlerin birbirine girmesi onlemek
+                      # ve okunabilir olmasini saglamak. 2 alt satir bu duruma ornektir
+echo ${string}abc
+echo $stringdoc     # Burda bos satir yazdiracaktir, cunku 'stringdoc' diye bir degisken yok.
+echo ${string}${integer}
+echo ${#string}     # "string" degiskenimizin icindeki degerin uzunlugunu ekrana basar.
+                        # Klasik len() fonksiyonu gibi
+                    # ${degisken:indis:uzunluk}
+                    # Bash'te karakter dizilerin indisleri 0 dan baslar.
+echo ${string:7}    # "string" degiskenimizin 7.ci indisinden baslayarak yazdirir.
+echo ${string:1:4}  # "string" degiskenimizin 1.ci indisinden baslayarak, 4 birim uzunlugunda karakter dizisi yazdirir.
